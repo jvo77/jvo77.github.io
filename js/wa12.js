6379f3ceadc8bb5d93c3f6a8bca231a7a404fbcd
jvo77 <jevo1113@colorado.edu>
2023-11-17 16:20:56 -0700

    inprogress

--- js/wa12.js
+++ js/wa12.js
@@ -1,19 +1,37 @@
 const newBtn = document.querySelector('#js-new-quote').addEventListener('click', getQuote);
-
+let change = document.getElementById("ptag");
 const questionTxt = document.querySelector('#js-quote-text');
 
 async function getQuote() {
     try{
-        const joke = fetch('https://icanhazdadjoke.com/',{
+        const joke = await fetch('https://icanhazdadjoke.com/',{
             headers: {
                 "Accept": "application/json"
             }
         })
-        const result = await joke.text();
-        console.log(result);
+        const result = await joke.json();
+        displayQuote(result);
+        displayQuote(result['joke']);
+        changeColor('yellow');
+        
+
     }
     catch(err) {
         console.log(err);
     }
 }
 
+function displayQuote(question) {
+    questionTxt.textContent = question;
+}
+
+function changeColor(color) {
+    document.body.style.background = color;
+}
+
+function myFunc() {
+    changeColor('yellow');
+} 
+
+
+getQuote();
