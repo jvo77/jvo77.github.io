b8c3906e2ce1d1136a26a62afb1dd1f733d5ce42
jvo77 <jevo1113@colorado.edu>
2023-12-11 15:05:22 -0700

    in progress

---
+++ final/final.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+  
+  var ball = {};
+  
+  ball.listofanswers = ["I guess so", "Yup, it is", "Yes, definitely", "Yes, duh", "Probably", "Yes", "I think so", "Try again", "Ask again", "Not sure", "Cannot think, retry", "Maybe ask again", "No", "Hmmm No", "NO", "I don't know, retry"];
+ 
+  $("#answer").hide();
+
+  ball.getAnswer = function()
+  {
+    var randomNumber = Math.random();
+    var randomAnswer = Math.floor(randomNumber * this.listofanswers.length);
+    var answer = this.listofanswers[randomAnswer];
+    
+    $("#answer").text(answer);
+    $("#answer").fadeIn(3000);
+    $("#8ball").effect( "shake");
+    $("#8ball").attr("src", "/img/8ballAnswer.png");
+
+    console.log(question);
+    console.log(answer);
+  };
+
+  ball.randomPhoneNumber = function()
+  {
+    var phoneNumber = Math.floor(100000000 + Math.random() * 900000000);
+    console.log(phoneNumber);
+    $("#phoneNumber").text(phoneNumber);
+  }
+
+  var onClick = function()
+  {
+    $("#answer").hide();
+    ball.getAnswer();
+
+    ball.randomPhoneNumber();
+  };
+  
+  $("#revealButton").click( onClick );
+
+});
